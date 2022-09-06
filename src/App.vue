8cb8c27a5449dacc42ed202a25084289d12bb315
WATA-Haru <hwatahiki@gmail.com>
2022-09-06 22:58:19 +0900

    add template test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
+    <myheader></myheader>
     <img src="./assets/logo.png">
     <router-view/>
+    <button class="btn btn-primary">test</button>
   </div>
 </template>
 
 <script>
+import myheader from './components/myheader'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    myheader
+  }
 }
 </script>
 
 <style>
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
